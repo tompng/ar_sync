31fc9729a18c4972c9e1becf992b9b9e4bf9de7e
tompng <tomoyapenguin@gmail.com>
2017-12-17 01:02:07 +0900

    set routes

--- sampleapp/config/routes.rb
+++ sampleapp/config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'foobar#show'
   get 'foobar/:key', to: 'foobar#broadcast'
+  root to: 'top#show'
+  resources :posts, except: :index do
+    post :reaction
+  end
+  resources :users, only: :index
+
+  resources :comments, only: [:create, :destroy] do
+    post :reaction
+  end
+
+  get '/followings', to: 'follows#followings'
+  get '/followeds', to: 'follows#followees'
+  resources :follows, only: [:create, :destroy]
+  resources :sessions, only: [:index, :new, :destroy]
 end
