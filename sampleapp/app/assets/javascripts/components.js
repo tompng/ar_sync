8564b0bdd43ad40d794a9b7a7fc3ea70c1e626f7
tompng <tomoyapenguin@gmail.com>
2017-12-18 02:36:27 +0900

    form styles

--- sampleapp/app/assets/javascripts/components.js
+++ sampleapp/app/assets/javascripts/components.js
@@ -1,12 +1,12 @@
 Vue.component('reactions', {
-  props: ['url', 'summary', 'mine'],
+  props: ['url', 'summary', 'mine', 'iconclass'],
   template: `
     <div>
       <a class='reaction-button' :class="{active: mine && mine.kind == 'like'}">
-        <i class='material-icons material-icons-large' @click='like'>thumb_up</i>
+        <i :class="'material-icons ' + iconclass" @click='like'>thumb_up</i>
       </a>
       <a class='reaction-button' :class="{active: mine && mine.kind == 'dislike'}">
-        <i class='material-icons material-icons-large' @click='dislike'>thumb_down</i>
+        <i :class="'material-icons ' + iconclass" @click='dislike'>thumb_down</i>
       </a>
       <small>
         <span v-if='summary.like'>
@@ -29,4 +29,31 @@ Vue.component('reactions', {
       fetch(this.url + '?kind=' + kind, { credentials: 'include', method: 'POST' })
     }
   }
+})
+
+Vue.component('comment-form', {
+  data() { return { text: '' } },
+  props: ['post_id'],
+  template: `
+    <form class='comment-form' @submit.prevent='send'>
+      <input v-model='text' placeholder='comment'>
+      <a @click='send'><i class='material-icons'>send</i></a>
+    </form>
+  `,
+  methods: {
+    send() {
+      if (!this.text) return
+      const url = '/comments?post_id=' + this.post_id
+      const headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+      const body = JSON.stringify({ comment: { body:  this.text }})
+      const option = { credentials: 'include', method: 'POST', headers, body }
+      fetch(url, option)
+      this.text = ''
+    }
+  }
+
+
 })
