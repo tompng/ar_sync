6c001bc61d0f85633dd8d4f28858303588ffa62a
tompng <tomoyapenguin@gmail.com>
2017-12-18 23:43:53 +0900

    turbolinks off

--- sampleapp/app/assets/javascripts/channels/sync.js
+++ sampleapp/app/assets/javascripts/channels/sync.js
@@ -1,13 +1,4 @@
-document.addEventListener('turbolinks:load', ()=>{
-
-
-})
-
-let currentSync = {}
-function syncStart(url, query) {
-  if (currentSync.vm) currentSync.vm.$destroy()
-  if (currentSync.subscriptions) currentSync.subscriptions.forEach(a => a.unsubscribe())
-  currentSync = {}
+function sync(url, query) {
   const headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
@@ -18,12 +9,12 @@ function syncStart(url, query) {
     const el = document.querySelector('.vue-root')
     syncdata.data.currentUser = JSON.parse(document.querySelector('#current_user').value)
     const store = new ARSyncStore(query, syncdata.data)
-    currentSync.subscriptions = syncdata.keys.map(key => {
+    syncdata.keys.map(key => {
       return App.cable.subscriptions.create(
         { channel: "SyncChannel", key },
         { received(patch) { store.update(patch) } }
       )
     })
-    currentSync.vm = new Vue({ el: '#syncbody', data: store.data })
+    new Vue({ el: '#syncbody', data: store.data })
   })
 }
