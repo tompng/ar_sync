c13901a5bc640d1fe2e08a0491ed10bfc5b1ec40
tompng <tomoyapenguin@gmail.com>
2017-12-19 01:04:59 +0900

    delete foobar_controller

--- sampleapp/config/routes.rb
+++ sampleapp/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'foobar/:key', to: 'foobar#broadcast'
   root to: 'top#show'
   resources :posts, except: :index do
     member do
