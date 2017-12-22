cecfea84b28ca90e2c31854d3ed20e2d752e8259
tompng <tomoyapenguin@gmail.com>
2017-12-23 04:49:11 +0900

    cleanup routes

--- sampleapp/config/routes.rb
+++ sampleapp/config/routes.rb
@@ -6,21 +6,12 @@ Rails.application.routes.draw do
   resources :posts, except: :index do
     member do
       post :reaction
-      get :sync_api
-      post :sync_api
     end
   end
-  resources :users, only: [:index, :show] do
-    member { get :sync_api; post :sync_api }
-    collection { get :profile_sync_api; post :profile_sync_api }
-  end
+  resources :users, only: [:index, :show]
 
   resources :comments, except: [:new, :index] do
-    member do
-      post :reaction
-      get :sync_api
-      post :sync_api
-    end
+    member { post :reaction }
   end
 
   get '/followings', to: 'follows#followings'
