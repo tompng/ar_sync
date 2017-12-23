c1d8c59c3bbeb5eeae90481a2bb5c689bbf6cae7
tompng <tomoyapenguin@gmail.com>
2017-12-24 00:15:10 +0900

    rm commentout

--- sampleapp/app/channels/sync_channel.rb
+++ sampleapp/app/channels/sync_channel.rb
@@ -2,8 +2,4 @@ class SyncChannel < ApplicationCable::Channel
   def subscribed
     stream_from params[:key]
   end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
 end
