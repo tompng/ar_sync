e740fee0b8b7cb35f98d22c3fb616d8db4d7595f
tompng <tomoyapenguin@gmail.com>
2017-12-26 23:04:40 +0900

    refactor testcases

--- test/ar_sync_test.rb
+++ test/ar_sync_test.rb
@@ -1,139 +1,71 @@
 database_name = 'test/test.sqlite3'
 ENV['DATABASE_NAME'] = database_name
 File.unlink database_name if File.exist? database_name
-
 require_relative 'seed'
 $patches = []
 ARSync.on_update do |key:, action:, path:, data:|
   $patches << { key: key, action: action, path: path, data: data }
 end
 
-begin
-  $jscode = []
-  def jsvar name, data
-    $jscode << "const #{name} = #{data.to_json}"
-  end
-  def jspatch name
-    jsvar name, $patches
-    $patches = []
-  end
-  query = [name: { as: '名前' }, posts: [:user, :title, as: :articles, my_comments: [:star_count, as: :my_opinions], comments: [:star_count, :user, my_stars: :id, my_star: { as: :my_reaction }]]]
-  post_query = [:user, :title, comments: [:body, as: :cmnts]]
-  collection_query = [:user, :title, my_comments: [:star_count, as: :my_opinions], comments: [:star_count, :user, my_stars: :id, my_star: { as: :my_reaction }]]
-  jsvar :query, query
-  jsvar :pquery, post_query
-  jsvar :cquery, collection_query
-  jsvar :initial, ARSync.sync_api(User.first, User.first, *query)
-  jsvar :pinitial, ARSync.sync_api(User.first.posts.first, User.first, *post_query)
-  jsvar :cinitial, ARSync.sync_collection_api(Post.sync_collection(:last10), User.first, *collection_query)
-  newpost = User.first.posts.create title: 'newposttitle', body: 'newpostbody', user: User.all.sample
-  User.first.posts.first.update title: 'title2'
-  newcomment1 = User.first.posts.first.comments.create body: 'newcomment1', user: User.all.sample
-  newcomment2 = User.first.posts.last.comments.create body: 'newcomment2', user: User.first
-  newpost.update title: 'newposttitle2'
-  jspatch :patches1
-  jsvar :data1, ARSync.sync_api(User.first, User.first, *query)[:data]
-  jsvar :pdata1, ARSync.sync_api(User.first.posts.first, User.first, *post_query)[:data]
-  jsvar :cdata1, ARSync.sync_collection_api(Post.sync_collection(:last10), User.first, *collection_query)[:data]
+query = [name: { as: '名前' }, posts: [:user, :title, as: :articles, my_comments: [:star_count, as: :my_opinions], comments: [:star_count, :user, my_stars: :id, my_star: { as: :my_reaction }]]]
+post_query = [:user, :title, comments: [:body, as: :cmnts]]
+collection_query = [:user, :title, my_comments: [:star_count, as: :my_opinions], comments: [:star_count, :user, my_stars: :id, my_star: { as: :my_reaction }]]
 
-  star1 = newcomment1.stars.create user: User.last
-  star2 = newcomment2.stars.create user: User.first
-  jspatch :patches2
-  jsvar :data2, ARSync.sync_api(User.first, User.first, *query)[:data]
-  jsvar :pdata2, ARSync.sync_api(User.first.posts.first, User.first, *post_query)[:data]
-  jsvar :cdata2, ARSync.sync_collection_api(Post.sync_collection(:last10), User.first, *collection_query)[:data]
-  star1.destroy
-  star2.destroy
-  newpost.destroy
-  User.first.posts.create title: 'newposttitle2', body: 'newpostbody2', user: User.all.sample
-  jspatch :patches3
-  jsvar :data3, ARSync.sync_api(User.first, User.first, *query)[:data]
-  jsvar :pdata3, ARSync.sync_api(User.first.posts.first, User.first, *post_query)[:data]
-  jsvar :cdata3, ARSync.sync_collection_api(Post.sync_collection(:last10), User.first, *collection_query)[:data]
-rescue => e
-  $error = e
-ensure
-  [star1, star2, newcomment1, newcomment2, newpost].each do |model|
-    model.destroy rescue nil
-  end
-  raise $error if $error
-  File.write 'test/generated_test.js', <<~CODE
-    const { ARSyncStore } = require('../index.js')
-    #{$jscode.join("\n")}
-    function compare(a, b, path, key){
-      if (!path) path = []
-      if (key) (path = [].concat(path)).push(key)
-      function withmessage(val){
-        if (!val) console.log(`${path.join('/')}: ${JSON.stringify(a)} != ${JSON.stringify(b)}`)
-        return val
-      }
-      if (a === b) return true
-      if (!a || !b) return withmessage(false)
-      if (a.constructor !== b.constructor) return withmessage(false)
-      if (a.constructor === Array) {
-        const len = Math.max(a.length, b.length)
-        for (let i=0; i<len; i++) {
-          if (i >= a.length || i >= b.length) {
-            console.log(`${path} at index ${i}: ${JSON.stringify(a[i])} != ${JSON.stringify(b[i])})}`)
-            return false
-          }
-          if (!compare(a[i], b[i], path, i)) return false
-        }
-      } else if (a.constructor === Object) {
-        const akeys = Object.keys(a).sort()
-        const bkeys = Object.keys(b).sort()
-        if (akeys.join('') != bkeys.join('')) {
-          console.log(`${path} keys: ${JSON.stringify(akeys)} != ${JSON.stringify(bkeys)}`)
-          return false
-        }
-        for (const i in a) {
-          if (!compare(a[i], b[i], path, i)) return false
-        }
-      } else {
-        return withmessage(a === b)
-      }
-      return true
-    }
-    function dup(obj) { return JSON.parse(JSON.stringify(obj)) }
-    function selectPatch(patches, keys) {
-      return dup(patches).filter(arr => keys.indexOf(arr.key) >= 0)
-    }
-    [true, false].forEach(immutable => {
-      const store = new ARSyncStore(query, dup(initial.data), { immutable })
-      store.batchUpdate(selectPatch(patches1, initial.keys))
-      console.log(compare(store.data, data1))
-      store.batchUpdate(selectPatch(patches2, initial.keys))
-      console.log(compare(store.data, data2))
-      store.batchUpdate(selectPatch(patches3, initial.keys))
-      console.log(compare(store.data, data3))
-
-      const pstore = new ARSyncStore(pquery, dup(pinitial.data), { immutable })
-      pstore.batchUpdate(selectPatch(patches1, pinitial.keys))
-      console.log(compare(pstore.data, pdata1))
-      pstore.batchUpdate(selectPatch(patches3, pinitial.keys))
-      console.log(compare(pstore.data, pdata2))
-      pstore.batchUpdate(selectPatch(patches3, pinitial.keys))
-      console.log(compare(pstore.data, pdata3))
-
-      const limit = cinitial.limit
-      const order = cinitial.order
-      const cstore = new ARSyncStore(cquery, dup(cinitial.data), { limit, order, immutable })
-      cstore.batchUpdate(selectPatch(patches1, cinitial.keys))
-      console.log(compare(cstore.data, cdata1))
-      cstore.batchUpdate(selectPatch(patches2, cinitial.keys))
-      console.log(compare(cstore.data, cdata2))
-      cstore.batchUpdate(selectPatch(patches3, cinitial.keys))
-      console.log(compare(cstore.data, cdata3))
-
-    })
-  CODE
-  output = `node test/generated_test.js`
-  puts output
-  passed, errors = output.lines.partition { |s| s.strip == 'true' }
-  if errors.present? || passed.empty?
-    puts "\e[31mFAILED\e[m"
-    exit(-1)
+$test_cases = {
+  user: [User.first, query],
+  post: [User.first.posts.first, post_query],
+  post_collection: [Post.sync_collection(:last10), collection_query]
+}
+def api(target, query)
+  if target.is_a? ActiveRecord::Base
+    ARSync.sync_api target.reload, User.first, *query
   else
-    puts "\e[32mPASSED\e[m"
+    ARSync.sync_collection_api target, User.first, *query
   end
+end
+
+$data = {
+  names: $test_cases.keys,
+  queries: $test_cases.values.map(&:last),
+  initials: $test_cases.values.map { |t, q| api t, q },
+  tests: []
+}
+def step_test!
+  $data[:tests] << {
+    patches: $patches,
+    states: $test_cases.values.map { |t, q| api(t, q)[:data] }
+  }
+  $patches = []
+end
+
+newpost = User.first.posts.create title: 'newposttitle', body: 'newpostbody', user: User.all.sample
+User.first.posts.first.update title: 'title2'
+newcomment1 = User.first.posts.first.comments.create body: 'newcomment1', user: User.all.sample
+newcomment2 = User.first.posts.last.comments.create body: 'newcomment2', user: User.first
+newpost.update title: 'newposttitle2'
+
+step_test!
+
+star1 = newcomment1.stars.create user: User.last
+star2 = newcomment2.stars.create user: User.first
+
+step_test!
+
+star1.destroy
+star2.destroy
+newpost.destroy
+User.first.posts.create title: 'newposttitle2', body: 'newpostbody2', user: User.all.sample
+
+step_test!
+
+File.write 'test/generated_test.js', "require('./test_helper.js')(#{$data.to_json})"
+output = `node test/generated_test.js`
+puts output
+passed = output.lines.grep(/true/)
+errors = output.lines.grep(/false/)
+if errors.present? || passed.empty?
+  puts "\e[31mFAILED\e[m"
+  exit(-1)
+else
+  puts "\e[32mPASSED\e[m"
 end
---
+++ test/test_helper.js
@@ -0,0 +1,71 @@
+const { ARSyncStore } = require('../index.js')
+function dup(obj) { return JSON.parse(JSON.stringify(obj)) }
+function selectPatch(patches, keys) {
+  return dup(patches).filter(arr => keys.indexOf(arr.key) >= 0)
+}
+
+function compareObject(a, b, path, key){
+  if (!path) path = []
+  if (key) (path = [].concat(path)).push(key)
+  function log(message) {
+    console.log(`${path.join('/')} ${message}`)
+  }
+  function withmessage(val){
+    if (!val) log(`${JSON.stringify(a)} != ${JSON.stringify(b)}`)
+    return val
+  }
+  if (a === b) return true
+  if (!a || !b) return withmessage(false)
+  if (a.constructor !== b.constructor) return withmessage(false)
+  if (a.constructor === Array) {
+    const len = Math.max(a.length, b.length)
+    for (let i=0; i<len; i++) {
+      if (i >= a.length || i >= b.length) {
+        log(`at index ${i}: ${JSON.stringify(a[i])} != ${JSON.stringify(b[i])})}`)
+        return false
+      }
+      if (!compareObject(a[i], b[i], path, i)) return false
+    }
+  } else if (a.constructor === Object) {
+    const akeys = Object.keys(a).sort()
+    const bkeys = Object.keys(b).sort()
+    if (akeys.join('') != bkeys.join('')) {
+      log(`keys: ${JSON.stringify(akeys)} != ${JSON.stringify(bkeys)}`)
+      return false
+    }
+    for (const i in a) {
+      if (!compareObject(a[i], b[i], path, i)) return false
+    }
+  } else {
+    return withmessage(a === b)
+  }
+  return true
+}
+
+function selectPatch(patches, keys) {
+  return dup(patches).filter(arr => keys.indexOf(arr.key) >= 0)
+}
+
+function executeTest({ names, queries, keysList, initials, tests }) {
+  for (const i in names) {
+    const query = queries[i]
+    const initial = initials[i]
+    const { limit, order } = initial
+    for (const immutable of [true, false]) {
+      console.log(`Test: ${names[i]}${immutable ? ' immutable' : ''}`)
+      try {
+        const store = new ARSyncStore(query, dup(initial.data), { immutable, limit, order })
+        for (const { patches, states } of tests) {
+          store.batchUpdate(selectPatch(patches, initial.keys))
+          const state = states[i]
+          console.log(compareObject(store.data, state))
+        }
+      } catch (e) {
+        console.log(e)
+        console.log(false)
+      }
+    }
+  }
+}
+
+module.exports = executeTest
