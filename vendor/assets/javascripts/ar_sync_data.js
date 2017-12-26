09fcc74c8caf23fc92d7b1bc772af99daaaf0171
tompng <tomoyapenguin@gmail.com>
2017-12-27 03:22:44 +0900

    buffered patch

--- vendor/assets/javascripts/ar_sync_data.js
+++ vendor/assets/javascripts/ar_sync_data.js
@@ -5,6 +5,7 @@ class ARSyncData {
     this.subscriptions = {}
     this.stores = {}
     this.vueData = {}
+    this.bufferedPatches = {}
   }
   release() {
     for (const subscription of this.subscriptions) {
@@ -22,15 +23,38 @@ class ARSyncData {
       if (callback) callback(this.vueData)
     })
   }
+  changed(callback) {
+    this.changedCallback = callback
+  }
+  patchReceived(name, patch) {
+    const buffer = this.bufferedPatches
+    ;(buffer[name] = buffer[name] || []).push(patch)
+    if (this.bufferTimer) return
+    this.bufferTimer = setTimeout(() => {
+      this.bufferTimer = null
+      const buf = this.bufferedPatches
+      this.bufferedPatches = {}
+      const changes = {}
+      for (const name in buf) {
+        changes[name] = this.stores[name].batchUpdate(buf[name])
+      }
+      if (this.changedCallback) this.changedCallback(changes)
+    }, 20)
+  }
   initializeStore(name, keys, data, option) {
     const query = this.requests[name].query
     const store = new ARSyncStore(query, data, option)
     this.stores[name] = store
     this.vueData[name] = store.data
+    let timer
+    let patches = []
+    const received = patch => {
+      this.patchReceived(name, patch)
+    }
     this.subscriptions[name] = keys.forEach(key => {
       return App.cable.subscriptions.create(
         { channel: "SyncChannel", key },
-        { received(patch) { store.update(patch) } }
+        { received }
       )
     })
   }
