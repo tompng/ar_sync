81f8c398c8f71d8edadf0cf771e324a2911f2cd0
tompng <tomoyapenguin@gmail.com>
2017-12-29 17:15:04 +0900

    use pure sub component, refactor react page

--- sampleapp/client/src/index.js
+++ sampleapp/client/src/index.js
@@ -2,9 +2,43 @@ import React, { PureComponent } from 'react'
 import { render } from 'react-dom'
 import { ARSyncImmutableData } from 'ar_sync'
 
-class TopPage extends PureComponent {
+class PostItem extends PureComponent {
   render() {
-    const { currentUser } = this.props
+    const { post } = this.props
+    return <div>
+      <a href={'/posts/' + post.id}>
+        <b>{post.title}</b>
+      </a>
+      &nbsp;&nbsp;
+      <small>date: {post.created_at} </small>
+      <small>&nbsp;&nbsp;comments:{post.comments_count}</small>
+    </div>
+  }
+}
+
+class TopPage extends PureComponent {
+  constructor(props, context) {
+    super(props, context)
+    this.state = null
+    const syncData = new ARSyncImmutableData({
+      currentUser: {
+        api: 'profile',
+        query: [
+          'name', 'followed_count', 'following_count',
+          { posts: ['title', 'comments_count', 'created_at'] }
+        ]
+      },
+      newPosts: {
+        api: 'newposts',
+        query: ['user', 'title', 'created_at', 'comments_count']
+      }
+    })
+    const update = () => this.setState(syncData.data)
+    syncData.load(update).changed(update)
+  }
+  render() {
+    if (!this.state) return <div>loading...</div>
+    const { currentUser } = this.state
     return <div>
       <h1>TopPage React Version: welcome {currentUser.name}!</h1>
       <h2>
@@ -14,38 +48,11 @@ class TopPage extends PureComponent {
       </h2>
       <hr/>
       <h2>Your posts</h2>
-      {currentUser.posts.map((post)=>{
-        return <div key={post.id}>
-          <a href={'/posts/' + post.id}>
-            <b>{post.title}</b>
-          </a>
-          &nbsp;&nbsp;
-          <small>date: {post.created_at} </small>
-          <small>&nbsp;&nbsp;comments:{post.comments_count}</small>
-        </div>
-      })}
+      {currentUser.posts.map(post => <PostItem key={post.id} post={post} />)}
     </div>
   }
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
-  const syncData = new ARSyncImmutableData({
-    currentUser: {
-      api: 'profile',
-      query: [
-        'name', 'followed_count', 'following_count',
-        { posts: ['title', 'comments_count', 'created_at'] }
-      ]
-    },
-    newPosts: {
-      api: 'newposts',
-      query: ['user', 'title', 'created_at', 'comments_count']
-    }
-  }).load(update).changed(update)
-  function update(){
-    render(
-      <TopPage {...syncData.data} />,
-      document.querySelector('#root')
-    )
-  }
+  render(<TopPage/>, document.querySelector('#root'))
 })
