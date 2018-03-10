56371384c0e8f46c5ae60049460551f0fe5ecd8f
tompng <tomoyapenguin@gmail.com>
2018-03-11 02:45:06 +0900

    last5comments

--- sampleapp/app/models/comment.rb
+++ sampleapp/app/models/comment.rb
@@ -5,6 +5,7 @@ class Comment < ApplicationRecord
 
   sync_self
   sync_parent :post, inverse_of: :comments
+  sync_parent :post, inverse_of: :comments_last5
   sync_parent :post, inverse_of: :comments_count
   sync_has_data :post_id, :body, :created_at, :updated_at
   sync_has_data(:user, includes: :user) { user.as_json(only: [:id, :name]) }
