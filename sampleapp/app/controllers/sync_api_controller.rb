ebc3ad938ed276b7ac5dc6e278b3f6b7d8f93155
tompng <tomoyapenguin@gmail.com>
2018-04-22 05:37:59 +0900

    sampleapp add resource api

--- sampleapp/app/controllers/sync_api_controller.rb
+++ sampleapp/app/controllers/sync_api_controller.rb
@@ -1,23 +1,23 @@
 class SyncApiController < ApplicationController
   include ArSync::ApiControllerConcern
 
+  api User do |ids|
+    User.where id: ids
+  end
+
+  api Post do |ids|
+    Post.where id: ids
+  end
+
+  api Comment do |ids|
+    Comment.where id: ids
+  end
+
   api :newposts do |_params|
     Post.sync_collection(:latest10)
   end
 
   api :profile do |_params|
     current_user
-  end
-
-  api :user do |params|
-    User.find params[:id]
-  end
-
-  api :post do |params|
-    Post.find params[:id]
-  end
-
-  api :comment do |params|
-    Comment.find params[:id]
   end
 end
