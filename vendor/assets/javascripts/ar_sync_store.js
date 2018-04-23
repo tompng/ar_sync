b4a6d6a054084299ae3c960613639e070a7cb650
tompng <tomoyapenguin@gmail.com>
2018-04-23 21:40:52 +0900

    js model & collection class

--- vendor/assets/javascripts/ar_sync_store.js
+++ vendor/assets/javascripts/ar_sync_store.js
@@ -41,113 +41,19 @@ const SyncBatchLoader = {
   }
 }
 
-class ArSyncModel {
-  constructor(query, data) {
-    if (data.collection) query = { attributes: { collection: query } }
-    this.query = query
-    this.data = {}
-    this.children = {}
-    this.paths = []
-    this.sync_keys = data.sync_keys
-    for (const key in data) {
-      const subData = data[key]
-      const subQuery = query.attributes[key]
-      if (key == 'sync_keys') continue
-      if (subQuery && subQuery.attributes && subQuery.attributes.sync_keys) {
-        this.paths.push(key)
-        if (subData instanceof Array) {
-          const models = subData.map(el => new ArSyncModel(subQuery, el))
-          this.children[key] = models
-          this.data[key] = models.map(m => m.data)
-        } else if(subData) {
-          const model = new ArSyncModel(subQuery, subData)
-          this.children[key] = model
-          this.data[key] = model.data
-        }
-      } else {
-        this.data[key] = subData
-      }
-    }
-    this.collection = this.data.collection
-    this.subscribe()
-  }
-  onnotify(path, notifyData) {
-    const { action, class_name, id } = notifyData
-    if (action == 'remove') {
-      if (this.children[path] instanceof Array) {
-        const idx = this.data[path].findIndex(a => a.id == id)
-        if (idx >= 0) {
-          this.children[path][idx].release()
-          this.children[path].splice(idx, 1)
-          this.data[path].splice(idx, 1)
-        }
-      } else {
-        this.children[path].release()
-        this.children[path] = null
-        this.data[path] = null
-      }
-    } else if (action == 'add') {
-      const query = this.query.attributes[path]
-      SyncBatchLoader.fetch(class_name, id, query).then((data) => {
-        const model = new ArSyncModel(query, data)
-        if (this.children[path] instanceof Array) {
-          this.children[path].push(model)
-          this.data[path].push(model.data)
-        } else {
-          this.children[path] = model
-          this.data[path] = model.data
-        }
-      })
-    } else {
-      SyncBatchLoader.fetch(class_name, id, this.reloadQuery()).then((data) => {
-        this.update(data)
-      })
-    }
-  }
-  reloadQuery() {
-    if (this.reloadQueryCache) return this.reloadQueryCache
-    const reloadQuery = this.reloadQueryCache = { attributes: [] }
-    for (const key in this.query.attributes) {
-      if (key == 'sync_keys') continue
-      const val = this.query.attributes[key]
-      if (!val || !val.attributes || !val.attributes.sync_keys) reloadQuery.attributes.push(key)
-    }
-    return reloadQuery
-  }
-  update(data) {
-    for (const key in data) {
-      this.data[key] = data[key]
-    }
+class ArSyncContainerBase {
+  constructor() {
+    this.listeners = []
   }
   release() {
-    this.unsubscribe()
-    const releaseElements = (elements) => {
-      for (const el of elements) {
-        if (el.constructor == ArSyncModel) {
-          el.release()
-        } else if (el.constructor == Array) {
-          releaseElements(el)
-        }
-      }
-    }
-    releaseElements(Object.values(this.data))
+    this.unsubscribeAll()
+    this.eachChild(child => child.release())
     this.data = null
   }
-  subscribe() {
-    this.listeners = []
-    const callback = data => this.onnotify(data)
-    if (!this.sync_keys) {
-      console.error('warning: no sync_keys')
-      return
-    }
-    for (const key of this.sync_keys) {
-      this.listeners.push(ArSyncSubscriber.subscribe(key, data => this.onnotify('', data)))
-      for (const path of this.paths) {
-        this.listeners.push(ArSyncSubscriber.subscribe(key + path, data => this.onnotify(path, data)))
-      }
-    }
+  subscribe(key, listener) {
+    this.listeners.push(ArSyncSubscriber.subscribe(key, listener))
   }
-  unsubscribe() {
+  unsubscribeAll() {
     for (const l of this.listeners) l.release()
     this.listeners = []
   }
@@ -208,9 +114,139 @@ class ArSyncModel {
   }
 }
 
+class ArSyncModel extends ArSyncContainerBase {
+  constructor(query, data){
+    super()
+    this.query = query
+    this.data = {}
+    this.children = {}
+    this.paths = []
+    this.sync_keys = data.sync_keys
+    if (!this.sync_keys) {
+      this.sync_keys = []
+      console.error('warning: no sync_keys')
+    }
+    for (const key in data) {
+      const subData = data[key]
+      const subQuery = query.attributes[key]
+      if (key == 'sync_keys') continue
+      if (subQuery && subQuery.attributes && subQuery.attributes.sync_keys) {
+        if (subData instanceof Array) {
+          const collection = new ArSyncCollection(this.sync_keys, key, subQuery, subData)
+          this.children[key] = collection
+          this.data[key] = collection.data
+        } else {
+          this.paths.push(key)
+          if (subData) {
+            const model = new ArSyncModel(subQuery, subData)
+            this.children[key] = model
+            this.data[key] = model.data
+          }
+        }
+      } else {
+        this.data[key] = subData
+      }
+    }
+    this.subscribeAll()
+  }
+  eachChild(callback) {
+    for (const key in this.children) callback(this.children[key])
+  }
+  onnotify(notifyData, path) {
+    const { action, class_name, id } = notifyData
+    if (action == 'remove') {
+      this.children[path].release()
+      this.children[path] = null
+      this.data[path] = null
+    } else if (action == 'add') {
+      const query = this.query.attributes[path]
+      SyncBatchLoader.fetch(class_name, id, query).then((data) => {
+        const model = new ArSyncModel(query, data)
+        if (this.children[path]) this.children[path].release()
+        this.children[path] = model
+        this.data[path] = model.data
+      })
+    } else {
+      SyncBatchLoader.fetch(class_name, id, this.reloadQuery()).then((data) => {
+        this.update(data)
+      })
+    }
+  }
+  subscribeAll() {
+    const callback = data => this.onnotify(data)
+    for (const key of this.sync_keys) this.subscribe(key, callback)
+    for (const path of this.paths) {
+      const pathCallback = data => this.onnotify(data, path)
+      for (const key of this.sync_keys) this.subscribe(key + path, pathCallback)
+    }
+  }
+  reloadQuery() {
+    if (this.reloadQueryCache) return this.reloadQueryCache
+    const reloadQuery = this.reloadQueryCache = { attributes: [] }
+    for (const key in this.query.attributes) {
+      if (key == 'sync_keys') continue
+      const val = this.query.attributes[key]
+      if (!val || !val.attributes || !val.attributes.sync_keys) reloadQuery.attributes.push(key)
+    }
+    return reloadQuery
+  }
+  update(data) {
+    for (const key in data) {
+      this.data[key] = data[key]
+    }
+  }
+}
+class ArSyncCollection extends ArSyncContainerBase {
+  constructor(sync_keys, path, query, data){
+    super()
+    if (sync_keys) {
+      this.sync_keys = sync_keys.map(key => key + path)
+    } else {
+      console.error('warning: no sync_keys')
+      this.sykc_keys = []
+    }
+    this.query = query
+    this.data = []
+    this.children = []
+    for (const subData of data) {
+      const model = new ArSyncModel(this.query, subData)
+      this.children.push(model)
+      this.data.push(model.data)
+    }
+    this.subscribeAll()
+  }
+  onnotify(notifyData) {
+    const { action, class_name, id } = notifyData
+    if (action == 'add') {
+      const query = this.query
+      SyncBatchLoader.fetch(class_name, id, query).then((data) => {
+        const model = new ArSyncModel(query, data)
+        this.children.push(model)
+        this.data.push(model.data)
+      })
+    } else if (action == 'remove') {
+      const idx = this.data.findIndex(a => a.id == id)
+      if (idx >= 0) {
+        this.children[idx].release()
+        this.children.splice(idx, 1)
+        this.data.splice(idx, 1)
+      }
+    }
+  }
+  eachChild(callback) {
+    for (const child of this.children) callback(child)
+  }
+  subscribeAll() {
+    const callback = data => this.onnotify(data)
+    for (const key of this.sync_keys) this.subscribe(key, callback)
+  }
+}
+
+
 try {
-  module.exports = ArSyncModel
+  module.exports = { ArSyncModel, ArSyncCollection }
 } catch (e) {
   window.ArSyncModel = ArSyncModel
+  window.ArSyncCollection = ArSyncCollection
 }
 })()
