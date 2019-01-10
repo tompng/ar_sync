3d0a37c9a3ebc4fb0651a11c1aeaad91d1181476
tompng <tomoyapenguin@gmail.com>
2019-01-10 15:48:09 +0900

    destroyed model propagation bugfix and reduce data for action destroy

--- lib/ar_sync/instance_methods.rb
+++ lib/ar_sync/instance_methods.rb
@@ -1,7 +1,8 @@
 module ArSync::InstanceMethods
   def _sync_notify(action)
     if self.class._sync_self?
-      ArSync.sync_send to: self, action: action, path: [], data: _sync_data
+      data = action == :destroy ? nil : _sync_data
+      ArSync.sync_send to: self, action: action, path: [], data: data
     end
     _sync_notify_parent action
   end
@@ -31,6 +32,15 @@ module ArSync::InstanceMethods
 
   def _sync_notify_parent(action, path: nil, data: nil, order_param: nil, only_to_user: nil)
     self.class._each_sync_parent do |parent, inverse_name:, only_to:|
+      parent = send(parent) if parent.is_a? Symbol
+      parent = instance_exec(&parent) if parent.is_a? Proc
+      next unless parent
+      next if parent.respond_to?(:destroyed?) && parent.destroyed?
+      inverse_name = instance_exec(&inverse_name) if inverse_name.is_a? Proc
+      next unless inverse_name
+      association_field = parent.class._sync_child_info inverse_name
+      next if association_field.skip_propagation? parent, self, path
+      action2 = association_field.action_convert action
       only_to_user2 = only_to_user
       if only_to
         to_user = only_to.is_a?(Symbol) ? instance_eval(&only_to) : instance_exec(&only_to)
@@ -38,16 +48,8 @@ module ArSync::InstanceMethods
         next if only_to_user && only_to_user != to_user
         only_to_user2 = to_user
       end
-      parent = send(parent) if parent.is_a? Symbol
-      parent = instance_exec(&parent) if parent.is_a? Proc
-      next unless parent
-      inverse_name = instance_exec(&inverse_name) if inverse_name.is_a? Proc
-      next unless inverse_name
-      association_field = parent.class._sync_child_info inverse_name
-      next if association_field.skip_propagation? parent, self, path
-      data2 = path ? data : association_field.data(parent, self, to_user: to_user, action: action)
+      data2 = path || action2 == :destroy ? data : association_field.data(parent, self, to_user: to_user, action: action)
       order_param2 = path ? order_param : association_field.order_param
-      action2 = association_field.action_convert action
       path2 = [*association_field.path(self), *path]
       ArSync.sync_send(
         to: parent, action: action2, path: path2, data: data2,
