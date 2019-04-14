0ea2f745f9b486816c883d2be3c8b9ea38085b83
tompng <tomoyapenguin@gmail.com>
2019-04-14 16:09:44 +0900

    add typed ArSyncModel.ts generator

--- lib/ar_sync/type_script.rb
+++ lib/ar_sync/type_script.rb
@@ -1,13 +1,18 @@
 module ArSync::TypeScript
+  def self.generate_typed_files(api_class, dir:, mode: nil)
+    mode ||= :graph if ActiveRecord::Base.include? ArSync::GraphSync
+    mode ||= :tree if ActiveRecord::Base.include? ArSync::TreeSync
+    {
+      'types.ts' => generate_type_definition(api_class),
+      'ArSyncModel.ts' => generate_model_script(mode)
+    }.each { |file, code| File.write File.join(dir, file), code }
+  end
+
   def self.generate_type_definition(api_class)
     [
       ArSerializer::TypeScript.generate_type_definition(api_related_classes(api_class)),
       request_type_definition(api_class)
     ].join "\n"
-  end
-
-  def self.generate_query_builder(api_class)
-    ArSerializer::TypeScript.generate_query_builder api_related_classes(api_class)
   end
 
   def self.api_related_classes(api_class)
@@ -18,24 +23,42 @@ module ArSync::TypeScript
   def self.request_type_definition(api_class)
     type = ArSerializer::GraphQL::TypeClass.from api_class
     definitions = []
-    request_type_names = []
+    request_types = {}
     type.fields.each do |field|
       association_type = field.type.association_type
       next unless association_type
       prefix = 'Class' if field.name.match? /\A[A-Z]/ # for class reload query
-      request_type_name = "SyncApi#{prefix}#{field.name.camelize}Request"
-      request_type_names << request_type_name
+      request_type_name = "Type#{prefix}#{field.name.camelize}Request"
+      request_types[field.name] = request_type_name
+      multiple = field.type.is_a? ArSerializer::GraphQL::ListTypeClass
       definitions << <<~CODE
         export interface #{request_type_name} {
           api: '#{field.name}'
           params?: #{field.args_ts_type}
           query: Type#{association_type.name}Query
+          _meta?: { data: Type#{field.type.association_type.name}#{'[]' if multiple} }
         }
       CODE
     end
     [
-      "export type SyncApiRequest = #{request_type_names.join ' | '}",
-      *definitions
+      'export type TypeRequest = ',
+      request_types.values.map { |value| "  | #{value}" },
+      'export type ApiNameRequests =  {',
+      request_types.map { |key, value| "  #{key}: #{value}" },
+      '}',
+      definitions
     ].join("\n")
+  end
+
+  def self.generate_model_script(mode)
+    <<~CODE
+      import { TypeRequest, ApiNameRequests } from './types'
+      import { DataTypeFromRequest } from 'ar_sync/DataType'
+      import ArSyncModelBase from 'ar_sync/#{mode}'
+      export default class ArSyncModel<R extends TypeRequest> extends ArSyncModelBase<{}> {
+        constructor(r: R) { super(r) }
+        data: DataTypeFromRequest<ApiNameRequests[R['api']], R> | {} | null
+      }
+    CODE
   end
 end
