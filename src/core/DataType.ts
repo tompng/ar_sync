d42583cd409742dfcde5cb1309d662652ad4d15e
tompng <tomoyapenguin@gmail.com>
2019-05-26 21:26:22 +0900

    change DataType for new query format

--- src/core/DataType.ts
+++ src/core/DataType.ts
@@ -16,37 +16,47 @@ interface ExtraFieldErrorType {
 
 type DataTypeExtractFromQueryHash<BaseType, QueryType> = '*' extends keyof QueryType
   ? {
-      [key in Exclude<(keyof BaseType) | (keyof QueryType), '_meta' | '_params' | '*'>]: (key extends keyof BaseType
-        ? (key extends keyof QueryType
-            ? (QueryType[key] extends true
-                ? DataTypeExtractField<BaseType, key>
-                : DataTypeFromQuery<BaseType[key] & {}, QueryType[key]>)
-            : DataTypeExtractField<BaseType, key>)
-        : ExtraFieldErrorType)
+      [key in Exclude<(keyof BaseType) | (keyof QueryType), '_meta' | '*'>]: (
+        key extends keyof QueryType
+        ? _DataTypePickField<BaseType, key, QueryType[key]>
+        : key extends keyof BaseType
+        ? DataTypeExtractField<BaseType, key>
+        : ExtraFieldErrorType
+      )
     }
-  : {
-      [key in keyof QueryType]: (key extends keyof BaseType
-        ? (QueryType[key] extends true
-            ? DataTypeExtractField<BaseType, key>
-            : DataTypeFromQuery<BaseType[key] & {}, QueryType[key]>)
-        : ExtraFieldErrorType)
-    }
+  : { [key in keyof QueryType]: _DataTypePickField<BaseType, key, QueryType[key]> }
+
+type _DataTypePickField<BaseType, Key, SubQuery> =
+  SubQuery extends { field: infer N, query?: infer Q }
+    ? (
+        N extends keyof BaseType
+        ? (
+            IsAnyCompareLeftType extends Q
+            ? DataTypeExtractField<BaseType, N>
+            : DataTypeFromQuery<BaseType[N], Q>)
+        : ExtraFieldErrorType
+      )
+    : (
+      Key extends keyof BaseType
+        ? (SubQuery extends true
+          ? DataTypeExtractField<BaseType, Key>
+          : DataTypeFromQuery<BaseType[Key], SubQuery>)
+        : ExtraFieldErrorType
+      )
 
 type _DataTypeFromQuery<BaseType, QueryType> = QueryType extends keyof BaseType | '*'
   ? DataTypeExtractFieldsFromQuery<BaseType, QueryType>
   : QueryType extends Readonly<(keyof BaseType | '*')[]>
   ? DataTypeExtractFieldsFromQuery<BaseType, Values<QueryType>>
-  : QueryType extends { as: string }
-  ? { error: 'type for alias field is not supported' } | undefined
   : DataTypeExtractFromQueryHash<BaseType, QueryType>
 
 export type DataTypeFromQuery<BaseType, QueryType> = BaseType extends any[]
   ? CheckAttributesField<BaseType[0], QueryType>[]
   : null extends BaseType
   ? CheckAttributesField<BaseType & {}, QueryType> | null
-  : CheckAttributesField<BaseType & {}, QueryType>
+  : CheckAttributesField<BaseType, QueryType>
 
-type CheckAttributesField<P, Q> = Q extends { attributes: infer R }
+type CheckAttributesField<P, Q> = Q extends { query: infer R }
   ? _DataTypeFromQuery<P, R>
   : _DataTypeFromQuery<P, Q>
 
@@ -70,7 +80,7 @@ type _ValidateDataTypeExtraFileds<Extra, Type> = SelectString<Values<Extra>> ext
   : { error: { extraFields: SelectString<Values<Extra>> } }
 type ValidateDataTypeExtraFileds<Type> = _ValidateDataTypeExtraFileds<CollectExtraFields<Type, []>, Type>
 
-type RequestBase = { api: string; query: any; params?: any; _meta?: { data: any } }
+type RequestBase = { field: string; query?: any; params?: any; _meta?: { data: any } }
 type DataTypeBaseFromRequestType<R> = R extends { _meta?: { data: infer DataType } } ? DataType : never
 export type DataTypeFromRequest<Req extends RequestBase, R extends RequestBase> = ValidateDataTypeExtraFileds<
   DataTypeFromQuery<DataTypeBaseFromRequestType<Req>, R['query']>
