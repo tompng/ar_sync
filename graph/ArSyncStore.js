dd2807783e4dc910b3fe01b220b7cbf4b8fe61f3
tompng <tomoyapenguin@gmail.com>
2019-09-21 20:45:38 +0900

    npm build

--- graph/ArSyncStore.js
+++ graph/ArSyncStore.js
@@ -377,16 +377,20 @@ class ArSyncCollection extends ArSyncContainerBase {
         const newData = [];
         for (const subData of collection) {
             let model = existings[subData.id];
+            let data = subData;
             if (model) {
                 model.replaceData(subData);
             }
-            else {
+            else if (subData.id) {
                 model = new ArSyncRecord(this.query, subData, null, this.root);
                 model.parentModel = this;
                 model.parentKey = subData.id;
             }
-            newChildren.push(model);
-            newData.push(model.data);
+            if (model) {
+                newChildren.push(model);
+                data = model.data;
+            }
+            newData.push(data);
         }
         while (this.children.length) {
             const child = this.children.pop();
