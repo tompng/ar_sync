fb5f34a5d99eef3abfff819c2c26a11e4a3da556
tompng <tomoyapenguin@gmail.com>
2019-11-27 02:37:59 +0900

    add minimum actioncable type definition

--- core/ActioncableAdapter.d.ts
+++ core/ActioncableAdapter.d.ts
@@ -1,10 +1,34 @@
 import ConnectionAdapter from './ConnectionAdapter';
+declare module ActionCable {
+    function createConsumer(): Cable;
+    interface Cable {
+        subscriptions: Subscriptions;
+    }
+    interface CreateMixin {
+        connected: () => void;
+        disconnected: () => void;
+        received: (obj: any) => void;
+    }
+    interface ChannelNameWithParams {
+        channel: string;
+        [key: string]: any;
+    }
+    interface Subscriptions {
+        create(channel: ChannelNameWithParams, obj: CreateMixin): Channel;
+    }
+    interface Channel {
+        unsubscribe(): void;
+        perform(action: string, data: {}): void;
+        send(data: any): boolean;
+    }
+}
 export default class ActionCableAdapter implements ConnectionAdapter {
     connected: boolean;
-    _cable: any;
-    actionCableClass: any;
-    constructor(actionCableClass: any);
-    subscribe(key: string, received: (data: any) => void): any;
+    _cable: ActionCable.Cable;
+    actionCableClass: typeof ActionCable;
+    constructor(actionCableClass: typeof ActionCable);
+    subscribe(key: string, received: (data: any) => void): ActionCable.Channel;
     ondisconnect(): void;
     onreconnect(): void;
 }
+export {};
--- src/core/ActioncableAdapter.ts
+++ src/core/ActioncableAdapter.ts
@@ -1,10 +1,34 @@
 import ConnectionAdapter from './ConnectionAdapter'
 
+declare module ActionCable {
+  function createConsumer(): Cable
+  interface Cable {
+    subscriptions: Subscriptions
+  }
+  interface CreateMixin {
+    connected: () => void
+    disconnected: () => void
+    received: (obj: any) => void
+  }
+  interface ChannelNameWithParams {
+    channel: string
+    [key: string]: any
+  }
+  interface Subscriptions {
+    create(channel: ChannelNameWithParams, obj: CreateMixin): Channel
+  }
+  interface Channel {
+    unsubscribe(): void;
+    perform(action: string, data: {}): void;
+    send(data: any): boolean;
+  }
+}
+
 export default class ActionCableAdapter implements ConnectionAdapter {
   connected: boolean
-  _cable: any
-  actionCableClass: any
-  constructor(actionCableClass: any) {
+  _cable: ActionCable.Cable
+  actionCableClass: typeof ActionCable
+  constructor(actionCableClass: typeof ActionCable) {
     this.connected = true
     this.actionCableClass = actionCableClass
     this.subscribe(Math.random().toString(), () => {})
