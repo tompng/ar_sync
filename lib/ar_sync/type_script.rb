5eccf080b0f4ef37319af2b77fa75264601d7dbd
tompng <tomoyapenguin@gmail.com>
2019-12-22 02:04:47 +0900

    have typescript generator itself (don't use ar_serializer's ts generator)

--- lib/ar_sync/type_script.rb
+++ lib/ar_sync/type_script.rb
@@ -10,8 +10,11 @@ module ArSync::TypeScript
   end
 
   def self.generate_type_definition(api_class)
+    classes = api_related_classes api_class
+    types = ArSerializer::TypeScript.related_serializer_types classes.flatten
     [
-      ArSerializer::TypeScript.generate_type_definition(api_related_classes(api_class)),
+      types.map { |t| data_type_definition t },
+      types.map { |t| query_type_definition t },
       request_type_definition(api_class)
     ].join "\n"
   end
@@ -97,5 +100,43 @@ module ArSync::TypeScript
         return useArSyncFetchBase<DataTypeFromRequest<R>>(request)
       }
     CODE
+  end
+
+  def self.query_type_definition(type)
+    field_definitions = type.fields.map do |field|
+      association_type = field.type.association_type
+      if association_type
+        qname = "Type#{association_type.name}Query"
+        if field.args.empty?
+          "#{field.name}?: true | #{qname} | { as?: string; attributes?: #{qname} }"
+        else
+          "#{field.name}?: true | #{qname} | { as?: string; params: #{field.args_ts_type}; attributes?: #{qname} }"
+        end
+      else
+        "#{field.name}?: true | { as: string }"
+      end
+    end
+    field_definitions << "'*'?: true"
+    query_type_name = "Type#{type.name}Query"
+    base_query_type_name = "Type#{type.name}QueryBase"
+    <<~TYPE
+      export type #{query_type_name} = keyof (#{base_query_type_name}) | Readonly<(keyof (#{base_query_type_name}))[]> | #{base_query_type_name}
+      export interface #{base_query_type_name} {
+      #{field_definitions.map { |line| "  #{line}" }.join("\n")}
+      }
+    TYPE
+  end
+
+  def self.data_type_definition(type)
+    field_definitions = []
+    type.fields.each do |field|
+      field_definitions << "#{field.name}: #{field.type.ts_type}"
+    end
+    field_definitions << "_meta?: { name: '#{type.name}'; query: Type#{type.name}QueryBase }"
+    <<~TYPE
+      export interface Type#{type.name} {
+      #{field_definitions.map { |line| "  #{line}" }.join("\n")}
+      }
+    TYPE
   end
 end
